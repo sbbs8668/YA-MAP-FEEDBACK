d1b89dc95666a93981a2f34b692caf54e6311448
sbbs8668 <sbbs8668@gmail.com>
2022-07-06 13:12:03 +0300

    webpack init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve('dist'),
+        clean: true,
+    },
+    devServer: {
+        index: 'index.html',
+        overlay: true,
+    },
+    devtool: 'source-map',
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Final project #1 Yandex maps API',
+        }),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['dist']
+        })
+    ],
+};
+
